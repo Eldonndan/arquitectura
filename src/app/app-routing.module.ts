3d5ef624dad1a4d4459c77643c8ad76dfca6412d
Damian <pokecodigos@gmail.com>
2023-11-07 20:59:13 -0300

    maqueta casi terminada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { UsuarioComponent } from './usuario/usuario.component';
+import { MisCitasComponent } from './mis-citas/mis-citas.component';
+import { AgendarHoraComponent } from './agendar-hora/agendar-hora.component';
+
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'usuario', component: UsuarioComponent},
+  {path: 'agendar', component: AgendarHoraComponent},
+  {path: 'citas', component: MisCitasComponent},
+
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { UsuarioComponent } from './usuario/usuario.component';
+import { AgendarHoraComponent } from './agendar-hora/agendar-hora.component';
+import { MisCitasComponent } from './mis-citas/mis-citas.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    UsuarioComponent,
+    AgendarHoraComponent,
+    MisCitasComponent
   ],
   imports: [
     BrowserModule,
